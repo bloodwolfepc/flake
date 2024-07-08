19a6a7c83f21af19aff899ddb9276f906f0e8aca
bloodwolfepc <bloodwolfepc@gmail.com>
2024-07-08 03:36:45 -0500

    commit

--- hosts/navi/default.nix
+++ hosts/navi/default.nix
@@ -5,7 +5,7 @@
       #inputs.hardware.nixosModules.<something>
       ./hardware-configuration.nix
       inputs.disko.nixosModules.default
-      ( import ../modules/option/disc/impermanence-btrfs.nix { device = "/dev/disk/by-id/ata-T-FORCE_240GB_TPBF2312190010101467"; })
+      ( import ../modules/option/disc/impermanence-btrfs.nix { device = "/dev/disk/by-id/ata-T-FORCE_240GB_TPBF2312190010101467"; })      
       ../modules/option/impermanence/impermanence-btrfs.nix
       ../modules/require
       ../modules/option/security/yubikey.nix
@@ -18,9 +18,13 @@
       ../modules/option/virtualization/docker.nix
     ];
   networking.hostName = "navi";
-  boot.loader.grub.enable = true;
-  boot.loader.grub.gfxmodeBios = "text";
-  boot.loader.grub.device = "/dev/disk/by-id/ata-T-FORCE_240GB_TPBF2312190010101467";
+  boot.loader.grub = { 
+    enable = true;
+    efiSupprt = true;
+    efiInstallAsRemovealbe = true;
+    gfxmodeBios = "text"; 
+  }; 
+  #boot.loader.grub.device = "/dev/disk/by-id/ata-T-FORCE_240GB_TPBF2312190010101467";
   networking.useDHCP = true;
   
 
