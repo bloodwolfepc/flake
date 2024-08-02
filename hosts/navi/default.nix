80d0d1e24cb18799caac5b6e119eb400a6a8a2bf
bloodwolfepc <bloodwolfepc@gmail.com>
2024-08-02 09:48:38 -0500

    commit

--- hosts/navi/default.nix
+++ hosts/navi/default.nix
@@ -4,18 +4,13 @@
     [       
       #inputs.hardware.nixosModules.<something>
       ./hardware-configuration.nix
-      inputs.disko.nixosModules.default
-      #( import ../modules/option/disc/impermanence-btrfs.nix { device = "/dev/sda"; })
-      # { device = "/dev/disk/by-id/ata-T-FORCE_240GB_TPBF2312190010101467"; })      
       ../modules/option/impermanence/impermanence-btrfs.nix
       ../modules/require
       ../modules/option/security/yubikey.nix
       ../modules/option/networking/ssh.nix
 
-
       ../modules/users/bloodwolfe
 
-      #../modules/option/server/nixarr.nix
       ../modules/option/virtualization/docker.nix
       ../modules/option/services/syncthing.nix
       ../modules/option/theme/stylix.nix
@@ -26,9 +21,8 @@
     device = "/dev/disk/by-id/ata-T-FORCE_240GB_TPBF2312190010101467";
     #efiSupport = true;
     #efiInstallAsRemovable = true;
-    gfxmodeBios = "text"; 
+    #gfxmodeBios = "text"; 
   }; 
-  #boot.loader.grub.device = "/dev/disk/by-id/ata-T-FORCE_240GB_TPBF2312190010101467";
   networking.useDHCP = true;
   sops.secrets = {
     "syncthing-key-navi" = {
--- hosts/navi/hardware-configuration.nix
+++ hosts/navi/hardware-configuration.nix
@@ -34,7 +34,7 @@
   fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/F511-1DCD";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      #options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices =
