d106e31b1030b79455d7380d68a6c94a8a543e4f
bloodwolfepc <bloodwolfepc@gmail.com>
2024-08-31 01:31:24 -0500

    commit

--- nixos/modules/extra-modules/flatpak.nix
+++ nixos/modules/extra-modules/flatpak.nix
@@ -1,3 +1,10 @@
 {
   services.flatpak.enable = true;
+	home.persistence."/persist/home/bloodwolfe" = {
+		allowOther = true;
+		directories = [
+      ".cache/flatpak"
+      ".local/share/flatpak"
+    ];
+  };
 }
--- nixos/modules/extra-modules/kde-connect.nix
+++ nixos/modules/extra-modules/kde-connect.nix
@@ -1,3 +1,9 @@
 {
   programs.kdeconnect.enable = true;
+	home.persistence."/persist/home/bloodwolfe" = {
+		allowOther = true;
+		directories = [
+      ".config/kdeconnect"
+    ];
+  };
 }
